e70619b9cf8f9b21fff1523a7f3e36e47adc819a
mirtcela <mirtcela@yandex.ru>
2023-02-10 14:51:02 +0300

    builder better solution

--- Builder/exercise.py
+++ Builder/exercise.py
@@ -1,17 +1,10 @@
-class CodeBuilder:
-    def __init__(self, root_name):
-        self.root_name = root_name
-        self.fields = []
-
-    def add_field(self, type="", name=""):
-        self.type = type
+class BasicClass():
+    def __init__(self, name):
         self.name = name
-        if self.type:
-            self.fields.append(f'self.{self.type} = {self.name}')
-        return self
-
+        self.fields = []
+    
     def __str__(self):
-        class_name = f' class {self.root_name}:'
+        class_name = f' class {self.name}:'
         init = '\n  def __init__(self):'
         field_str = ''
         for f in self.fields:
@@ -20,6 +13,25 @@ class CodeBuilder:
             return class_name + init+ field_str
         else:
             return class_name + '\n  pass'
+
+class Field():
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def __str__(self):
+        return f'self.{self.name} = {self.value}'
+    
+class CodeBuilder:
+    def __init__(self, root_name):
+        self.__class = BasicClass(root_name)
+
+    def add_field(self, type, name):
+        self.__class.fields.append(Field(type, name))
+        return self
+
+    def __str__(self):
+        return self.__class.__str__()
     
 cb = CodeBuilder('Person').add_field('name', '""').add_field('age', '0')
 print(cb)
